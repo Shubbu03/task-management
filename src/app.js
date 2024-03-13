9c95180350999251897fdfe21a6a1ec362bdf994
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-03-13 11:49:32 +0530

    Add cookie-parser and cors dependencies

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//MIDDLEWARES =>
+app.use(express.json({ limit: "16kb" })); // limit to set how much data is allowed on server through JSON at a time
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //extended to allow nested objects.
+app.use(express.static("public")); // static to store files / favicons in a local folder
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error occured!!", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      `Server is running at port ${process.env.PORT}`;
+    });
+  })
+  .catch((err) => {
+    console.log(`DB Connection error:: ${err}`);
+  });
